ffa4a3a265382541e946078020bc49a673bd462e
Mariano Pérez Rodríguez <mariano.perez.rodriguez@gmail.com>
2023-03-15 18:34:56 -0300

    React to messages based on channel IDs

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,36 @@
-import { GatewayIntentBits, Message, Partials } from 'discord.js'
-import { Client } from 'discord.js'
+import { Client, Events, GatewayIntentBits, Message } from 'discord.js'
+
+import getReactionMap from './config/reaction-map'
+import { ReactionEntry } from './model/ReactionEntry'
 
 require('dotenv').config()
 
+let reactionMap: ReactionEntry[]
+
 const client = new Client({
-    partials: [
-        Partials.Message,
-        Partials.Channel,
-        Partials.Reaction,
-    ],
     intents: [
-        GatewayIntentBits.DirectMessages,
-        GatewayIntentBits.DirectMessageReactions,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMessageReactions,
         GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.MessageContent,
     ]
 })
 
-client.on('ready', () => {
+client.on(Events.MessageCreate, async (message: Message) => {
+    if (message.guildId == process.env.DISCORD_GUILD_ID) {
+        const channelId: string = (await message.channel.fetch()).id
+        reactionMap.forEach(async (entry: ReactionEntry) => {
+            if (entry.channelId == channelId) {
+                entry.reactions.map(async (emoji: string) => {
+                    await message.react(emoji)
+                })
+            }
+        })
+    }
+})
+
+client.once(Events.ClientReady, async () => {
+    reactionMap = await getReactionMap()
     console.log('Discord bot ready')
 })
 
